49a1131e0322cf1aed9c0b00e703ce184ea910ef
Nikita <nikappleid@yandex.ru>
2022-03-08 22:01:24 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+import MainSlider from './modules/slider/slider-main';
+import MiniSlider from './modules/slider/slider-mini';
+import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
+import Form from './modules/forms';
+import Accordion from './modules/accordion';
+import Download from './modules/download';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+   const slider = new MainSlider({
+       btns: '.next', 
+       container: '.page'
+    }); 
+   slider.render();
+
+
+   const modulePageSLider = new MainSlider({
+       container: '.moduleapp',
+       btns: '.next'
+    });
+
+    modulePageSLider.render();
+
+   const showUpSlider = new MiniSlider({
+       container: '.showup__content-slider',
+       next: '.showup__next',
+       prev: '.showup__prev',
+       activeClass: 'card-active',
+       animate: true
+
+   });
+   showUpSlider.init();
+
+   const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev',
+        activeClass: 'card-active',
+        animate: true,
+        autoPlay: true
+   });
+   modulesSlider.init();
+
+   const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active'
+    });
+    feedSlider.init();
+    
+    new VideoPlayer('.showup .play', '.overlay').init();
+    new VideoPlayer('.module__video-item .play', '.overlay').init();
+
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
+
+    new Form('.form').init();
+
+    new Accordion('.plus__content').init();
+
+    new Download('.download').init();
+});
---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,95 @@
+import Slider from './slider';
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev, activeClass, animate, autoPlay, paused) {
+        super(container, next, prev, activeClass, animate, autoPlay, paused);
+    }
+
+    decorizeSlides() {
+        this.slides.forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if(this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+        });
+
+        if (!this.slides[0].closest('button')) {
+            this.slides[0].classList.add(this.activeClass);
+        }
+
+        if(this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+    nextSlide() {
+        if(this.slides[1].tagName == "BUTTON" && this.slides[2].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]);
+            this.container.appendChild(this.slides[1]);
+            this.container.appendChild(this.slides[2]);
+            this.decorizeSlides();
+        } else if (this.slides[1].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]);
+            this.container.appendChild(this.slides[1]);
+            this.decorizeSlides();
+        } else {
+            this.container.appendChild(this.slides[0]);
+            this.decorizeSlides();
+            this.decorizeSlides();
+        }
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
+
+        this.prev.addEventListener('click', () => {
+
+            for(let i = this.slides.length - 1; i > 0; i--) {
+                if (this.slides[i].tagName !== "BUTTON") {
+                    let active = this.slides[i];
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlides();
+                    break;
+                }
+            }
+
+            
+        }); 
+    }
+
+    activateAnimation() {  
+        
+        if (this.autoPlay) {
+            this.paused = setInterval(() => this.nextSlide(), 2000);
+        }
+
+        
+    }
+
+
+    init() {
+            try {
+                this.container.style.cssText = `
+                display: flex;
+                flex-wrap: wrap;
+                overflow: hidden;
+                align-items: flex-start;
+            `;
+
+            this.bindTriggers();
+            this.decorizeSlides();
+            this.activateAnimation();
+
+            this.slides[0].parentNode.addEventListener('mouseenter', () => {
+                clearInterval(this.paused);
+            });
+
+            this.slides[0].parentNode.addEventListener('mouseleave', () => {
+                this.activateAnimation();
+            });
+        } catch(e) {}
+
+    }
+}
